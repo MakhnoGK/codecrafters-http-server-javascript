6c697c11a8d88dbc296ef4c14686944599026472
Hryhorii Khyzhniakov <hryhorii@bloomag.com>
2023-11-05 11:37:42 +0200

    pass stage 3

--- app/main.js
+++ app/main.js
@@ -1,8 +1,44 @@
 const net = require('net')
 
+class Request {
+    constructor(method, path, version) {
+        this.method = method
+        this.path = path
+        this.version = version
+    }
+
+    static parse(str) {
+        const [method, path, version] = str.split(/\s/)
+
+        return new Request(method, path, version)
+    }
+}
+
+class RequestHeaders {
+    constructor(request) {
+        this.request = request
+    }
+
+    static parse(buffer) {
+        const str = buffer.toString('utf-8')
+        const [request] = str.split(/\r\n/)
+
+        return new RequestHeaders(
+            Request.parse(request)
+        )
+    }
+}
+
 const server = net.createServer((socket) => {
-    socket.on('data', () => {
-        socket.write('HTTP/1.1 200 OK \r\n\r\n', 'utf-8')
+    socket.on('data', (data) => {
+        const headers = RequestHeaders.parse(data)
+
+        if (headers.request.path === '/') {
+            socket.write('HTTP/1.1 200 OK\r\n\r\n', 'utf-8')
+        } else {
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n', 'utf-8')
+        }
+
         socket.end()
     })
 
