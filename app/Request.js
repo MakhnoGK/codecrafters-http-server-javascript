e0b8792d5f5b4a854952df582f41a92411263905
Hryhorii Khyzhniakov <hryhorii@bloomag.com>
2023-11-05 15:27:12 +0200

    refactor request handling

---
+++ app/Request.js
@@ -0,0 +1,57 @@
+class RequestParser {
+    constructor(buffer) {
+        this.parts = buffer.toString().split('\r\n')
+    }
+
+    getRequestInfo() {
+        const [request] = this.parts
+        const [method, path, version] = request.split(/\s/)
+
+        return { method, path, version }
+    }
+
+    getUserAgent() {
+        const uaEntry = this.parts.find((search) => search.toLowerCase().includes('user-agent'))
+        const [, ua] = uaEntry.match(/User-Agent:\s(\S+)/) ?? []
+
+        return ua;
+    }
+
+    getBody() {
+       return this.parts[this.parts.length - 1]
+    }
+}
+
+class Request {
+    method
+    path
+    version
+    ua
+    data
+
+    constructor(method, path, version, ua, data) {
+        this.method = method
+        this.path = path
+        this.version = version
+        this.ua = ua
+        this.data = data
+    }
+
+    static parse(buffer) {
+        const parser = new RequestParser(buffer)
+
+        const ua = parser.getUserAgent()
+        const { method, path, version } = parser.getRequestInfo()
+        const data = parser.getBody()
+
+        return new Request(
+            method,
+            path,
+            version,
+            ua,
+            data
+        )
+    }
+}
+
+module.exports = Request
--- app/main.js
+++ app/main.js
@@ -2,43 +2,7 @@ const net = require('net')
 const fs = require('fs')
 const path = require('path')
 
-class Request {
-    constructor(method, path, version) {
-        this.method = method
-        /** @type {string} */
-        this.path = path
-        this.version = version
-    }
-
-    static parse(str) {
-        const [method, path, version] = str.split(/\s/)
-
-        return new Request(method, path, version)
-    }
-}
-
-class RequestHeaders {
-    constructor(request, ua, data) {
-        this.request = request
-        this.ua = ua
-        this.data = data;
-    }
-
-    static parse(buffer) {
-        const str = buffer.toString('utf-8')
-        const parts = str.split(/\r\n/)
-        const [request] = parts;
-        const [, ua] = str.match(/User-Agent:\s(\S+)/) ?? []
-
-        const data = parts[parts.length - 1];
-
-        return new RequestHeaders(
-            Request.parse(request),
-            ua,
-            data
-        )
-    }
-}
+const Request = require('./Request')
 
 const textResponse = (text) => {
     return (
@@ -57,60 +21,76 @@ const fileResponse = (bytesRead, buffer) => {
 }
 
 const fileCreatedResponse = () => {
-
     return (
         'HTTP/1.1 201 Created\r\n\r\n'
     )
 }
 
+
 const notFoundResponse = () => {
     return 'HTTP/1.1 404 Not Fund\r\n\r\n'
 }
 
+const handlers = {
+    '/': function () {
+        this.socket.write('HTTP/1.1 200 OK\r\n\r\n', 'utf-8')
+    },
+    '/echo': function () {
+        const [, text] = this.headers.path.match(/\/echo\/(.+)/)
+
+        this.socket.write(textResponse(text), 'utf-8')
+    },
+    '/user-agent': function () {
+        this.socket.write(textResponse(this.headers.ua), 'utf-8')
+    },
+    '/files': function () {
+        const [, filename] = this.headers.path.match(/\/files\/(.+)/)
+
+        const dirFlag = process.argv.indexOf('--directory') + 1
+        const directory = process.argv[dirFlag]
+
+        const filePath = path.join(directory, filename)
+
+        try {
+            if (this.headers.method === 'GET') {
+                const fd = fs.openSync(filePath, 'r')
+                const contents = fs.readFileSync(fd)
+
+                this.socket.write(fileResponse(contents.length, contents))
+            } else if (this.headers.method === 'POST') {
+                fs.writeFileSync(filePath, this.headers.data)
+                this.socket.write(fileCreatedResponse())
+            }
+        } catch (error) {
+            this.socket.write(notFoundResponse())
+        }
+    }
+}
+
+const getHandler = (path, handlers) => {
+    const [, routeName] = path.match(/^(\/\w*)?/)
+
+    return handlers[routeName]
+}
+
 const server = net.createServer((socket) => {
     socket.on('data', (data) => {
-        const headers = RequestHeaders.parse(data)
+        const headers = Request.parse(data)
+        const handler = getHandler(headers.path, handlers)
 
-        if (headers.request.path.startsWith('/files')) {
-            const [, filename] = headers.request.path.match(/\/files\/(.+)/)
-            const dirFlag = process.argv.indexOf('--directory') + 1;
-            const directory = process.argv[dirFlag];
-            const file = path.join(directory, filename);
+        if (!handler) {
+            socket.write(notFoundResponse())
+            socket.end();
 
-            try {
-                if (headers.request.method === 'GET') {
-                    const fd = fs.openSync(file, 'r');
-                    const contents = fs.readFileSync(fd)
-
-                    socket.write(fileResponse(contents.length, contents))
-                } else if (headers.request.method === 'POST') {
-                    fs.writeFileSync(file, headers.data)
-                    socket.write(fileCreatedResponse())
-                }
-            } catch(error) {
-                socket.write(notFoundResponse())
-            }
-        } else if (headers.request.path.startsWith('/user-agent')) {
-            socket.write(textResponse(headers.ua), 'utf-8')
-        } else if (headers.request.path.startsWith('/echo')) {
-            const [, text] = headers.request.path.match(/\/echo\/(.+)/)
-
-            socket.write(
-                textResponse(text),
-                'utf-8'
-            )
-        } else if (headers.request.path === '/') {
-            socket.write('HTTP/1.1 200 OK\r\n\r\n', 'utf-8')
-        } else {
-            socket.write(notFoundResponse(), 'utf-8')
+            return;
         }
 
+        handler.call({ socket, headers })
         socket.end()
     })
 
     socket.on('close', () => {
         socket.end()
-        // server.close()
     })
 })
 
