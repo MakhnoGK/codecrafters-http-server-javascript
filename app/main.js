778c7f08d05085d5f7fa2f32276c22f113fbd30c
Hryhorii Khyzhniakov <hryhorii@bloomag.com>
2023-11-05 11:05:28 +0200

    pass 2nd stage

--- app/main.js
+++ app/main.js
@@ -1,6 +1,11 @@
 const net = require('net')
 
 const server = net.createServer((socket) => {
+    socket.on('connect', () => {
+        socket.write('HTTP/1.1 200 OK \r\n\r\n', 'utf-8')
+        socket.end()
+    })
+
     socket.on('close', () => {
         socket.end()
         server.close()
