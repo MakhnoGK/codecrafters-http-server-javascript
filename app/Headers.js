fac75a9d1e29922d8c3528b20ba1313089bcee91
Hryhorii Khyzhniakov <hryhorii@bloomag.com>
2023-11-05 17:49:29 +0200

    fix ua parsing

--- app/Headers.js
+++ app/Headers.js
@@ -12,7 +12,7 @@ class RequestParser {
 
     getUserAgent() {
         const uaEntry = this.parts.find((search) => search.toLowerCase().includes('user-agent:'))
-        const [, ua] = uaEntry.split(': ')
+        const [, ua] = uaEntry?.split(': ') ?? []
 
         return ua;
     }
